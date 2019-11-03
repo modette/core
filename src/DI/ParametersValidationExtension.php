e62f5a22b0caf3b549f1f8216a9251f83a67fd02
Marek Bartoš <bartos.developer152@gmail.com>
2019-11-03 18:18:26 +0100

    [Core] DI: parameters validation

---
+++ src/DI/ParametersValidationExtension.php
@@ -0,0 +1,122 @@
+<?php declare(strict_types = 1);
+
+namespace Modette\Core\DI;
+
+use Modette\Core\Boot\Configurator;
+use Modette\Exceptions\Logic\InvalidStateException;
+use Nette\DI\CompilerExtension;
+
+class ParametersValidationExtension extends CompilerExtension
+{
+
+	public function loadConfiguration(): void
+	{
+		$parameters = $this->getContainerBuilder()->parameters;
+
+		$this->checkDebugMode($parameters);
+		$this->checkConsoleMode($parameters);
+		$this->checkDirectories($parameters);
+		$this->checkModulesMeta($parameters);
+	}
+
+	/**
+	 * @param mixed[] $parameters
+	 */
+	private function checkDebugMode(array $parameters): void
+	{
+		$debugMode = $this->checkParameterExistence($parameters, 'debugMode');
+		$productionMode = $this->checkParameterExistence($parameters, 'productionMode');
+
+		if ($debugMode === $productionMode) {
+			throw new InvalidStateException(sprintf(
+				'Parameters \'%s\' and \'%s\' must be exact opposite, but their values are identical.',
+				'debugMode',
+				'productionMode'
+			));
+		}
+	}
+
+	/**
+	 * @param mixed[] $parameters
+	 */
+	private function checkConsoleMode(array $parameters): void
+	{
+		$consoleMode = $this->checkParameterExistence($parameters, 'consoleMode');
+		$httpMode = $this->checkParameterExistence($parameters, 'httpMode');
+
+		if ($consoleMode === $httpMode) {
+			throw new InvalidStateException(sprintf(
+				'Parameters \'%s\' and \'%s\' must be exact opposite, but their values are identical.',
+				'consoleMode',
+				'httpMode'
+			));
+		}
+	}
+
+	/**
+	 * @param mixed[] $parameters
+	 */
+	private function checkDirectories(array $parameters): void
+	{
+		// TODO - appDir validation requires proper scope configuration in monorepo
+		$dirs = ['rootDir', /*'appDir',*/ 'logDir', 'tempDir', 'vendorDir'];
+
+		foreach ($dirs as $dirName) {
+			$dirValue = $this->checkParameterExistence($parameters, $dirName);
+
+			if (!is_dir($dirValue)) {
+				throw new InvalidStateException(sprintf(
+					'Parameter \'%s\' must contain path to an existing directory, \'%s\' given.',
+					$dirName,
+					$dirValue
+				));
+			}
+		}
+	}
+
+	/**
+	 * @param mixed[] $parameters
+	 */
+	private function checkModulesMeta(array $parameters): void
+	{
+		$modules = $this->checkParameterExistence($parameters, 'modules');
+		$rootDir = $parameters['rootDir'];
+
+		foreach ($modules as $moduleName => $moduleMeta) {
+			$relativeDir = $moduleMeta['dir'];
+			$absoluteDir = $rootDir . '/' . $relativeDir;
+			$key = 'modules > ' . $moduleName . ' > dir';
+
+			if (!is_dir($absoluteDir)) {
+				throw new InvalidStateException(sprintf(
+					'Parameter \'%s\' must contain path to an existing directory, \'%s\' given.',
+					$key,
+					$relativeDir
+				));
+			}
+		}
+	}
+
+	/**
+	 * @param mixed[] $parameters
+	 * @return mixed
+	 */
+	private function checkParameterExistence(array $parameters, string $parameterName)
+	{
+		if (!isset($parameters[$parameterName])) {
+			throw new InvalidStateException(sprintf(
+				'Mandatory parameter \'%s\' is missing. %s',
+				$parameterName,
+				$this->getSuggestion()
+			));
+		}
+
+		return $parameters[$parameterName];
+	}
+
+	private function getSuggestion(): string
+	{
+		return sprintf('Use \'%s\' to load DI container.', Configurator::class);
+	}
+
+}
